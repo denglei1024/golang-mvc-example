6627e61a96d86dacdaa02caa4fd1f912eb6f5223
denglei <denglei1024@foxmail.com>
2023-07-09 00:01:31 +0800

    ✨ feat: 添加创建产品的API

--- controllers/catalogController.go
+++ controllers/catalogController.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/techdenglei/eshop/initiailizers"
+	"github.com/techdenglei/eshop/models"
 )
 
 type CatalogController struct {
@@ -14,6 +16,7 @@ func (catalog *CatalogController) Router(c *gin.Engine) {
 	c.GET("/catalogbrands", catalogBrands)
 	c.GET("/items", items)
 	c.GET("/items/:id", itemById)
+	c.POST("/items", createProduct)
 }
 
 func catalogTypes(c *gin.Context) {
@@ -30,4 +33,19 @@ func items(c *gin.Context) {
 
 func itemById(c *gin.Context) {
 	c.String(http.StatusOK, "from item by id")
+}
+
+func createProduct(c *gin.Context) {
+	var product models.CatalogItem
+	c.BindJSON(&product)
+	result := initiailizers.DB.Create(&product)
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": result.Error,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"product": product,
+	})
 }
